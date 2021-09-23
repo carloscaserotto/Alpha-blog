bf5f545445a01f716b8e1cd621686495ec377c33
Chino <carlos.caserotto@yahoo.com>
2021-09-23 12:29:38 -0300

    Add edit

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
   get "articles", to: "articles#index"
 
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end

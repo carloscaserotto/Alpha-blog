0d5908ab3975a0af01e540eb3c35f60d25a29adf
Chino <carlos.caserotto@yahoo.com>
2021-09-29 08:45:24 -0300

    Commit before get older version

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,11 @@
 import { environment } from '@rails/webpacker'
 
-import { ProvidePlugin } from "webpack"
+const webpack = require("webpack")
 
-environment.plugins.append("Provide", new ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
 }))
 
 export default environment

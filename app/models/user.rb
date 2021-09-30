984744f312e6c0174dca61e37b2cdd8536c24b73
Chino <carlos.caserotto@yahoo.com>
2021-09-30 14:20:53 -0300

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: {case_sensitive: false}#, length: {minimun: 3}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true,uniqueness: {case_sensitive: false}, 
+                format: { with: VALID_EMAIL_REGEX} #, length: { maximun: 105}
 end

44984cbfe76f9b7099103598ff9d9fbfd6bb4b4a
Chino <carlos.caserotto@yahoo.com>
2021-10-01 15:22:09 -0300

    Add edit template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+
     def create
         @user = User.new(user_params)
         if @user.save   
@@ -11,6 +16,18 @@ class UsersController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Your account information was successfully updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+
+
     end
 
     private

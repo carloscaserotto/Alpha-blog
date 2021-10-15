cf64626377dd36c6d03df24e0cd351cc485af4b9
Chino <carlos.caserotto@yahoo.com>
2021-10-15 14:14:51 -0300

    Add create sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
-    
+    def index
+        @users = User.paginate(page: params[:page], per_page: 5)
+    end
     def new
         @user = User.new
     end
@@ -14,12 +16,13 @@ class UsersController < ApplicationController
 
 
     def create
+        #byebug
         @user = User.new(user_params)
         if @user.save   
-            flash[:notice] = "Welcome to Alpha-Blog #{@user.username}, your have successfully sign-up"
-            redirect_to articles_path
+           flash[:notice] = "Welcome to Alpha-Blog #{@user.username}, your have successfully sign-up"
+           redirect_to articles_path
         else
-            render 'new'
+           render 'new'
         end
     end
 
@@ -27,7 +30,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information was successfully updated"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
